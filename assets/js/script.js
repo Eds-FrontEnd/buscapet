27ca69b4513c254f8c06d3b11844faf8318cb503
Edson-Dev <edsfrontend10@gmail.com>
2024-06-19 23:12:13 -0300

    ajustes nos arquivos

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,14 +10,13 @@ const buscarPet = async () => {
     const dog = await api.json();
 
     if (dog.status == "error" || input.value == "" || input.value === null || input.value === undefined) {
-        result.innerHTML = `
-        <img src="../../assets/img/error404.png" />
-        `;
-        
+        result.innerHTML = `<img src="../../assets/img/error404.png" />`;
         
         setTimeout(() => {
             input.value="";
         }, 1000);
+
+        alert("Ops, tente novamente...");
 
     } else {
         try {
@@ -37,8 +36,14 @@ const printDog = (dog) => {
     setTimeout(() => {
         input.value = "";
     }, 1000);
+    
+    c(".loading").style.display="flex";
 
-    for (let i in dog) {
+    setTimeout(()=>{
+        c(".loading").style.display="none";
+    }, 1500);
+
+    for(let i in dog) {
         result.innerHTML += `<img src=${dog[i]} class="imgPet" />`;
     }
 };
@@ -50,3 +55,9 @@ btnBuscarPet.addEventListener("click", buscarPet);
 // Teclar Enter na Busca
 const pressEnter = c("input");
 pressEnter.addEventListener("change", buscarPet);
+
+window.onload = function(){
+    setTimeout(()=>{
+        c(".loading").style.display="none";
+    }, 1500);
+}
